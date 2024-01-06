85a12a29a36735b94d969189e8090569e33d9964
hagicode <131940602+hagicode@users.noreply.github.com>
2024-01-07 01:35:37 +0900

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -1,21 +1,14 @@
 import streamlit as st
-from gsheetsdb import connect
-import pandas as pd
-conn = connect()
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    return rows
-sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNs1x9X5JI8KXYu2-_YkhlNBpuBwhRqCoTleQyRfJrVjDnhbOxWI1GPBp8RlQBoNhhqgCjDXnDWI67/pub?gid=0&single=true&output=csv"
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-# スプレッドシートの内容をstreamlitに表示する
-st.write('''## スプレッドシートの内容を行ごとに表示する''')
-for row in rows: st.write(f"{row}")
-# それぞれの列の情報を個別に抽出する
-st.write('''## それぞれの列の情報を個別に抽出する''')
-for row in rows: st.write(f"{row.日付}の{row.名前}の{row.値段}円です。")
-# データフレームに変換し表示する
-st.write('''## データフレームに変換し表示する''')
-row_list = []
-for row in rows: row_list.append(row)
-df=pd.DataFrame(row_list)
-st.table(df)
+from streamlit_gsheets import GSheetsConnection
+
+##共有設定後にurlの末尾に/edit?usp=sharingをつける。
+url = "https://docs.google.com/spreadsheets/d/1vXaglvGGbGN0pc8vEjiA7bCPpPTacFxvLGm3iKRVVUw//edit?usp=sharing"
+
+# こっちで動作
+#conn = st.experimental_connection("gsheets", type=GSheetsConnection) 
+
+# こっちでModuleNotFoundError: No module named 'streamlit_gsheets'が発生
+conn = st.connection("gsheets", type=GSheetsConnection) 
+
+data = conn.read(spreadsheet=url)
+st.dataframe(data)
